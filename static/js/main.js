0ee109c94e437801da476e26742e8ae9a61062f9
Tony-Otieno <nitonito598@gmail.com>
2024-11-06 14:24:01 +0300

    initial commit

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+// Example JavaScript for smooth scroll or interactive features
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('My Django Blog is loaded!');  // Test log for JS
+
+    // You can add more interactive features here like smooth scrolling, animations, etc.
+});
